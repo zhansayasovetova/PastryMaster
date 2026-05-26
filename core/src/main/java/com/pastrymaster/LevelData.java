359fc97c6986a92c15526ccd2d4bb52ef491ea40
keneskarakat <karakat.kenes@narxoz.kz>
2026-05-26 09:02:09 +0500

    create level data and recipe configuration

---
+++ core/src/main/java/com/pastrymaster/LevelData.java
@@ -0,0 +1,21 @@
+package com.pastrymaster;
+
+public class LevelData {
+
+    public static String[] desserts = {
+        "Pancake",
+        "Croissant",
+        "Strawberry Cake",
+        "Cupcake",
+        "Chocolate Cheesecake"
+    };
+
+    public static String[] customerOrders = {
+        "I want fluffy pancakes!",
+        "Please make a crispy croissant!",
+        "Can I get a strawberry cake?",
+        "I'd love a cute cupcake!",
+        "Make me a chocolate cheesecake!"
+    };
+
+}
